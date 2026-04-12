d0fab0faaf448504ff15d21791d3160b03c2ccaf
Mervynlinc <lincmervynz4@gmail.com>
2026-04-13 00:52:26 +0300

    add: vercel analytics

--- landing-page/app/layout.tsx
+++ landing-page/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, DM_Sans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-display",
